4105f71ecd9fa509e04152d7f8858709b14dadda
1280007175@qq.com <SWsw802300>
2017-04-01 11:16:56 +0800

    笔记

--- app/src/main/java/com/example/administrator/advanceday11/MainActivity.java
+++ app/src/main/java/com/example/administrator/advanceday11/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.administrator.advanceday11;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * setting->plugins->Browse repositor
+ * ignore
+ * gsonformat
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
